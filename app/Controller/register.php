02202ae4882a19630b5338d9c532f4a72d1a2e40
Blooh123 <bloohgamedev@gmail.com>
2025-07-03 11:05:14 +0800

    da

--- app/Controller/register.php
+++ app/Controller/register.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $data = json_decode(file_get_contents("php://input"), true);
 if (!$data) exit('No data');
 
@@ -6,16 +10,22 @@ $username = preg_replace('/[^a-zA-Z0-9_]/', '', $data['username']);
 $imgNum = intval($data['imgNum']);
 $imgData = $data['imgData'];
 
-// Save images to public/assets/js/labels/{username}
+// Directory to save images
 $dir = __DIR__ . '/../../public/assets/js/labels/' . $username;
-if (!is_dir($dir)) mkdir($dir, 0777, true);
+if (!is_dir($dir)) {
+    if (!mkdir($dir, 0777, true)) {
+        exit('Failed to create directory: ' . $dir);
+    }
+}
 
 $img = str_replace('data:image/jpeg;base64,', '', $imgData);
 $img = str_replace(' ', '+', $img);
 $fileData = base64_decode($img);
 
-file_put_contents("$dir/$imgNum.jpg", $fileData);
+if (file_put_contents("$dir/$imgNum.jpg", $fileData) === false) {
+    exit('Failed to save file: ' . "$dir/$imgNum.jpg");
+}
 
-echo "Saved $dir/$imgNum.jpg";
+echo "<script>alert('Image saved successfully!');</script>";
 
 
