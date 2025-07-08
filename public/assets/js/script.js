02906d5baba76267252acc282b0e7d732ec27fdc
Blooh123 <bloohgamedev@gmail.com>
2025-07-09 06:34:38 +0800

    good UI

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -147,7 +147,7 @@ const statusEl = document.getElementById('status');
 
 function setStatus(message, statusClass) {
   statusEl.textContent = message;
-  statusEl.className = `status-message ${statusClass}`;
+  statusEl.className = `px-4 py-2 mb-4 rounded-lg font-medium bg-green-100 text-green-600 shadow`;
 }
 
 // When starting scanning
