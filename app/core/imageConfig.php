c075738a4c3381829f5e920d73ea0a762164574b
Blooh123 <bloohgamedev@gmail.com>
2025-07-11 18:14:18 +0800

    da

--- app/core/imageConfig.php
+++ app/core/imageConfig.php
@@ -27,6 +27,6 @@ $imagePath5 = ROOT."assets/Images/Default.png";
 $imageData5 = base64_encode(file_get_contents($imagePath5));
 $imageSource5 = 'data:image/png;base64,' . $imageData5;
 
-$imagePath6 = ROOT."assets/Images/bilat.png";
+$imagePath6 = ROOT."assets/Images/bilat.jpg";
 $imageData6 = base64_encode(file_get_contents($imagePath6));
 $imageSource6 = 'data:image/png;base64,' . $imageData6; 
