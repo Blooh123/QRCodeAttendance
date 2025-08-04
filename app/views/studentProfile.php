11c8bb00a9c224dd8c04799dcff0cac4ccda0c2e
Blooh123 <bloohgamedev@gmail.com>
2025-08-04 14:13:09 +0800

    heill

--- app/views/studentProfile.php
+++ app/views/studentProfile.php
@@ -200,7 +200,7 @@ require_once '../app/core/imageConfig.php'; // Include your configuration file
                                     <span class="font-semibold text-[#a31d1d]">Fullstack Developer, UI/UX Designer, Database Administrator</span>
                                 </div>
                                 <div class="flex items-center justify-center md:justify-start gap-2 mt-2">
-                                    <span class="text-gray-600 text-base italic">"Takbong pogi"</span>
+                                    <span class="text-gray-600 text-base italic">Takbong pogi</span>
                                     <svg class="h-5 w-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20">
                                         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.967a1 1 0 00.95.69h4.175c.969 0 1.371 1.24.588 1.81l-3.38 2.455a1 1 0 00-.364 1.118l1.287 3.966c.3.922-.755 1.688-1.54 1.118l-3.38-2.454a1 1 0 00-1.175 0l-3.38 2.454c-.784.57-1.838-.196-1.54-1.118l1.287-3.966a1 1 0 00-.364-1.118L2.049 9.394c-.783-.57-.38-1.81.588-1.81h4.175a1 1 0 00.95-.69l1.286-3.967z"/>
                                     </svg>
