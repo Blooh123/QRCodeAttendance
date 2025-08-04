ab764a5f2bc4a6c06212a9464e90b527d68fc297
Blooh123 <bloohgamedev@gmail.com>
2025-08-04 14:23:23 +0800

    da

--- app/views/landingPage.php
+++ app/views/landingPage.php
@@ -178,8 +178,8 @@ require "../app/core/imageConfig.php";
         <div class="max-w-4xl mx-auto" data-aos="zoom-in" data-aos-duration="1000">
             <h3 class="text-2xl font-bold text-gray-900 mb-6 text-center">System Overview</h3>
             <div class="video-container">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/F8xpmTmdf6o?si=saEwbDG79HkpeZHW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/l7Kj-QySG9s?si=gr0H2bRiXAbbWtdc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> -->
+                <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/F8xpmTmdf6o?si=saEwbDG79HkpeZHW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> -->
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/l7Kj-QySG9s?si=gr0H2bRiXAbbWtdc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
         </div>
     </section>
