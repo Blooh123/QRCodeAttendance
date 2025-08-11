9c61b0845cdf6c264206de4f731e2614b6cedaf2
Blooh123 <bloohgamedev@gmail.com>
2025-08-12 07:50:18 +0800

    da

--- app/views/login.php
+++ app/views/login.php
@@ -67,7 +67,7 @@ global $imageSource, $imageSource2, $imageSource4, $imageSource7;
     <div class="w-full max-w-4xl flex items-center justify-center mb-8">
         <div class="floating">
             <img 
-                src="<?php echo $imageSource4 ?>" 
+                src="<?php echo $imageSource7 ?>" 
                 alt="Logo" 
                 class=" backdrop-blur-sm hover:transform hover:scale-105 transition-all duration-300"
             />
