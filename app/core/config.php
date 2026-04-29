7d7134e25f179bcac4b2edd85375a87402d1f44c
eaglearn <bloohgamedev@gmail.com>
2026-04-29 18:53:01 +0800

    sa

--- app/core/config.php
+++ app/core/config.php
@@ -10,9 +10,9 @@ if($_SERVER['SERVER_NAME'] == 'localhost'){
     defined('ROOT') or define("ROOT", 'https://localhost/QRCodeAttendance/QRCodeAttendance/public/');
 
 }else{
-    define('DBNAME', 'u753706103_qr_attendance');//u753706103_qr_attendance
-    define('DBUSER', 'u753706103_christian');//u753706103_christian
-    define('DBPASS', 'mZ2~G76JP1s5=B=Cy1L*');//mZ2~G76JP1s5=B=Cy1L*
+    define('DBNAME', 'u711652065_qr_attendance');//u753706103_qr_attendance
+    define('DBUSER', 'u711652065_christian');//u753706103_christian
+    define('DBPASS', '&zWxj4$G7');//&zWxj4$G7
     define('DBHOST', 'localhost');
     define('DBPORT', '3306');
     defined('ROOT') or define("ROOT", 'https://usepqrattendance.site/public/');
